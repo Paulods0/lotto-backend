e48d579e093c3abc0761dfddc9f1a022bae52d29
paulods0 <pauloluguenda0@gmail.com>
2025-08-04 22:46:51 +0100

    chore: add redis env variables

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,20 +1,22 @@
-import z from "zod";
+import z from 'zod';
 
 const envSchema = z.object({
-    JWT_ACCESS_TOKEN_SECRET:z.string(),
-    JWT_REFRESH_TOKEN_SECRET:z.string(),
-    JWT_EXPIRES_IN:z.string(),
-    PORT:z.number().default(3333),
-    DATABASE_URL:z.string(),
-})
+  JWT_ACCESS_TOKEN_SECRET: z.string(),
+  JWT_REFRESH_TOKEN_SECRET: z.string(),
+  JWT_EXPIRES_IN: z.string(),
+  PORT: z.number().default(3333),
+  DATABASE_URL: z.string(),
+  REDIS_HOST: z.string(),
+  REDIS_PASSWORD: z.string(),
+});
 
-const parsed = envSchema.safeParse(process.env)
+const parsed = envSchema.safeParse(process.env);
 
-if(!parsed.success){
-    console.log("Enviroment variables error: ", parsed.error)
-    throw new Error("Enviroment variable is required: ", parsed.error)
+if (!parsed.success) {
+  console.log('Enviroment variables error: ', parsed.error);
+  throw new Error('Enviroment variable is required: ', parsed.error);
 }
 
-const env = parsed.data
+const env = parsed.data;
 
-export default env
+export default env;
--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,16 +1,20 @@
-import Redis from "ioredis";
+import Redis from 'ioredis';
+import env from '../constants/env';
 // 172.17.198.67
 // host: "172.17.198.67",
 // password: "msftsrep0."
 
-const redis = new Redis();
-
-redis.on("ready", async () => {
-    console.log("✅ Conectado ao Redis com sucesso!");
+const redis = new Redis({
+  host: env.REDIS_HOST,
+  password: env.REDIS_PASSWORD,
 });
 
-redis.on("error", (err) => {
-    console.error("❌ Erro ao conectar ao Redis:", err);
+redis.on('ready', async () => {
+  console.log('✅ Conectado ao Redis com sucesso!');
+});
+
+redis.on('error', err => {
+  console.error('❌ Erro ao conectar ao Redis:', err);
 });
 
 export default redis;
