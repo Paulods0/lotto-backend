9e3ab006c82db909b35ceacdfc0f3c14b932ade3
paulods0 <pauloluguenda0@gmail.com>
2025-08-08 16:25:21 +0100

    fetch terminal on pos.agent

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,8 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// const allowedOrigins = ['https://mtjogos.co.ao'];
-const allowedOrigins = ['*', 'http://localhost:5173'];
+const allowedOrigins = ['https://mtjogos.co.ao'];
+// const allowedOrigins = ['*', 'http://localhost:5173'];
 
 app.use(
   cors({
